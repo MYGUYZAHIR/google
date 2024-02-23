d27187b263a13221b6aec407a1ffdfac52b23c4e
MYGUYZAHIR <124379015+MYGUYZAHIR@users.noreply.github.com>
2024-02-23 08:48:24 -0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -12,7 +12,7 @@ module.exports = (req, res) => {
   //   }
 
   createProxyMiddleware({
-    target,
+    google.com,
     changeOrigin: true,
     pathRewrite: {
       // rewrite request path `/backend`
